57539812326903615ea1235686c9c5018de2e85d
SWFTR <mirko.scheschonka@gmail.com>
2021-05-06 12:15:18 +0200

    managed to fix input bug

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 
+let imageSearch = `
+<nav>
+    <div class='small-header-part-left'>
+        <div class='josefin logo-margin'><span style="color: #ea4335; font-size: 24px;">E</span><span style="color: #34A853; font-size: 24px;" >l</span><span style="color: #4285f4; font-size: 24px;">g</span><span style="color: #fbbc05; font-size: 24px;">o</span><span style="color: #ea4335; font-size: 24px;">o</span><span style="color: #4285f4; font-size: 24px;">g</span></div>
+    </div>
+    <div id="input-container">
+        <input placeholder="Search for Images" id='page-input'>
+        <button type="submit" onclick='pageSearchImages()' id='page-button'>
+            <span class="material-icons icon-size" >
+                search
+            </span>
+        </button>
+    </div>
+    <div class='small-header-part-right'></div>
+</nav>
+<div id='behind-header'></div>
+<div id='images-container'>
 
-
+</div>
+<script src="script.js"></script>
+`
 
 function displayData(arr) {
     let imagesContainer = document.querySelector('#images-container');
@@ -16,8 +35,14 @@ function displayData(arr) {
         </div>`;
 
         imagesContainer.innerHTML += image;
-
     }
+
+
+}
+
+function searchImages() {
+    getData(search.value)
+    console.log(search.value)
 }
 
 
@@ -33,21 +58,42 @@ async function getData(query) {
 }
 
 
-var search = document.querySelector("input");
-
+let search = document.querySelector("#home-input");
 
 search.addEventListener("keydown", function(event) {
 
   if (event.keyCode === 13) {
     event.preventDefault();
-    document.querySelector("button").click();
+    document.querySelector("#home-button").click();
   }
 });
 
-function searchImages() {
+let pageSearch;
+
+function showImages() {
+    let body = document.querySelector('body');
+    body.innerHTML = '';
+    body.innerHTML = imageSearch;
     getData(search.value)
+
+    pageSearch = document.querySelector("#page-input");
+
+    pageSearch.addEventListener("keydown", function(event) {
+
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.querySelector("#page-button").click();
+    }
+  });
+   
 }
 
 function myFunction(event) { 
     event.target.parentNode.remove();
-  }
+}
+
+function pageSearchImages() {
+    getData(pageSearch.value);
+}
+
+
